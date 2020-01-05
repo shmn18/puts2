0113ca843761f719b41044daab067a8d1320f5a5
shmn18 <55428899+shmn18@users.noreply.github.com>
2020-01-05 15:10:57 +0100

    addition logic added

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
+from fractions import Fraction
+
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET','POST'])
 def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
+    value1=request.args.get('A',default = 0, type = Fraction)
+    value2=request.args.get('A',default = 0, type = Fraction)
     result=value1+value2
-    return '%d \n' % result
+    x=str(result).split('/')
+    if len(x) == 2:
+        y=float(x[0])/float(x[1])
+        z=str(y).split(".")
+        if z[1] == '0':
+            return " %s\n" %z[0]
+        else:
+            return " %s\n" %y
 
+    else:
+        n=str(result).split(".")
+        return " %s \n" % n[0]
+    
 
-if __name__ == "__main__":
-    app.run()
